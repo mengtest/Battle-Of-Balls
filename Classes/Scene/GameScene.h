27c05deb93f8a51173bf02a7fa55f2af59c3d729
17521 <39322918+17521@users.noreply.github.com>
2018-06-24 00:42:46 +0800

    Update GameScene.h

--- Classes/Scene/GameScene.h
+++ Classes/Scene/GameScene.h
@@ -1,22 +1,26 @@
-#ifndef _GameScene_H_
+ #ifndef _GameScene_H_
 #define _GameScene_H_
 
 #include "cocos2d.h"
 #include "ui/CocosGUI.h"
 
+class Player;
 
 USING_NS_CC;
 using namespace ui;
 
 class GameScene : public Layer {
 public:
-	static Scene * createScene();
+	static Scene * createScene();  //创建场景
 
-	virtual bool init();  //初始化函数
+	virtual bool init();  //初始化
 
 	CREATE_FUNC(GameScene);
 
-	void menuSpitCallback(Ref * pSender, CheckBox::EventType type);   //吐孢子回调
+
+	void menuSpitCallback(Ref * pSender, CheckBox::EventType type);  //吐孢子回调
 	void menuDivideCallback(Ref * pSender, CheckBox::EventType type);  //分身回调
+
+	Player * _player;
 };
 #endif
